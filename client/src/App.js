9c25fcc10ddf315a562aa49dd1b5aefbb87759a3
Sapphire Smith <ssapphire527@gmail.com>
2023-05-05 14:28:16 +0530

    Add backend url in client side

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ function App() {
 
   // Fetching message from backend on mount
   useEffect(() => {
-    fetch("http://localhost:4000")
+    fetch("https://mern-deployment-backend.onrender.com")
       .then((res) => res.json())
       .then((data) => setMessage(data.message));
   }, []);

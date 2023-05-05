2a7095c8b31ae516cc1b0a6d6593f9e26e5b5c29
Sapphire Smith <ssapphire527@gmail.com>
2023-05-05 14:07:50 +0530

    Initial commit Done

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const [message, setMessage] = useState("");
+
+  // Fetching message from backend on mount
+  useEffect(() => {
+    fetch("http://localhost:4000")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>message</h1>
+      <h3>{message}</h3>
+    </div>
+  );
+}
+
+export default App;
